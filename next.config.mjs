7c85372c106de36008e7b9cb9617d3cc1ef329f3
Ansh Saxena <anshsaxena4190@gmail.com>
2025-09-19 08:28:36 +0530

    standardization, removed ai slops, fixed/added links

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/docs/virtual-machines/pre-configured-vms',
+        destination: '/docs/virtual-machines/vm-templates',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
